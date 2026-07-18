619faf0f832b6bbeedf220d97df4788941367a49
yarpenart <karolass11@gmail.com>
2026-07-18 12:30:28 +0200

    check errors in hooks

--- scripts/constants/constants.js
+++ scripts/constants/constants.js
@@ -1,25 +1,50 @@
-export const MODULE_ID = "desperate-measures";
+export const MODULE_ID =
+  "desperate-measures";
 
-export const MODULE_NAME = "Desperate Measures";
+export const MODULE_NAME =
+  "Desperate Measures";
 
 export const SETTINGS = {
-  BLOODIED_THRESHOLD: "bloodiedThreshold",
-  ALLOW_ZERO_HP: "allowZeroHP",
-  REQUIRE_COMBAT: "requireCombat",
-  RESET_ON_LONG_REST: "resetOnLongRest",
-  SHOW_CHAT_MESSAGES: "showChatMessages"
+  BLOODIED_THRESHOLD:
+    "bloodiedThreshold",
+
+  ALLOW_ZERO_HP:
+    "allowZeroHP",
+
+  REQUIRE_COMBAT:
+    "requireCombat",
+
+  RESET_ON_LONG_REST:
+    "resetOnLongRest",
+
+  SHOW_CHAT_MESSAGES:
+    "showChatMessages"
 };
 
 export const FLAGS = {
-  BLOODIED: "bloodied",
-  PENDING_EFFECTS: "pendingEffects"
+  BLOODIED:
+    "bloodied",
+
+  PENDING_EFFECTS:
+    "pendingEffects"
 };
 
 export const MEASURE_IDS = {
-  DASH_DISENGAGE: "dashDisengage",
-  PLUS_FIVE: "plusFive",
-  REROLL_ATTACK: "rerollAttack",
-  MAXIMIZE_DAMAGE: "maximizeDamage",
-  EXTRA_ACTION: "extraAction",
-  RECOVER_SPELL_SLOT: "recoverSpellSlot"
+  DASH_DISENGAGE:
+    "dashDisengage",
+
+  PLUS_FIVE:
+    "plusFive",
+
+  REROLL_ATTACK:
+    "rerollAttack",
+
+  MAXIMIZE_DAMAGE:
+    "maximizeDamage",
+
+  EXTRA_ACTION:
+    "extraAction",
+
+  RECOVER_SPELL_SLOT:
+    "recoverSpellSlot"
 };
